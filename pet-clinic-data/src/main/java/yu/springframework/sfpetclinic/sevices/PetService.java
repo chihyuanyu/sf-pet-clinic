40d4b78996b3ef4558e2f2c1699aca779934e5b6
chihyuanyu <chihyuanyu1@gmail.com>
2021-03-08 20:12:55 +0800

    update interface

---
+++ pet-clinic-data/src/main/java/yu/springframework/sfpetclinic/sevices/PetService.java
@@ -0,0 +1,15 @@
+package yu.springframework.sfpetclinic.sevices;
+
+import yu.springframework.sfpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
